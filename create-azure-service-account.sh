e90d46b3c582edc39dfc013ee4e3f72c7f27a46c
Fabian Lee <flee999@hotmail.com>
2021-05-29 21:27:26 +0000

    bash version of creating service account

--- create-azure-service-account.sh
+++ create-azure-service-account.sh
@@ -5,47 +5,47 @@
 #
 
 path_to_az=$(which az)
-if [ -x "$path_to_az" ]; then
-  echo "ERROR could not find azure CLI see: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt"
+if [ ! -x "$path_to_az" ]; then
+  echo "ERROR could not find azure CLI: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt"
   exit 1
 fi
 
-az account list
-echo $?
-
-exit 0
-loginlist=$(az account list --query [].user)
-if ($loginlist.Length -lt 6) {
-    write-host "run 'az login' first"
-    exit 1
-}
+# check for login
+az account list > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "ERROR, run 'az login' first"
+  exit 2
+fi
 
 # if logged in, we can get subscription and tenant id
-$me=az account show --query user.name -o tsv
-write-host "logged in: $me"
-$subscriptionId=az account show --query id -o tsv
-$tenantId=az account show --query tenantId -o tsv
-write-host "subscription/tenant = $subscriptionId/$tenantId"
+me=$(az account show --query user.name -o tsv)
+echo "logged in: $me"
+subscriptionId=$(az account show --query id -o tsv)
+tenantId=$(az account show --query tenantId -o tsv)
+echo "subscription/tenant = $subscriptionId/$tenantId"
 
 # if sp app exists, we can get app id, uri, and name
-$appId=az ad app list --query [].appId -o tsv
-if (! $appId ) {
-    write-host "sp app does not exist, need to create"
-    $thepassword=az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscriptionId" --out tsv --query password
-}else {
-    write-host "sp app already exists, recreating with new password"
-    $thepassword=az ad sp credential reset --name $appId --out tsv --query password
-}
+appId=$(az ad app list --query [].appId -o tsv)
+if [ -z "$appId" ]; then
+    echo "sp app does not exist, need to create"
+    thepassword=$(az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscriptionId" --out tsv --query password)
+else
+    echo "sp app already exists, recreating with new password"
+    thepassword=$(az ad sp credential reset --name $appId --out tsv --query password)
+fi
 
-$appId=az ad app list --query [].appId -o tsv
-$displayName=az ad app list --query [].displayName -o tsv
-$appUri=az ad app list --query [].identifierUris[0] -o tsv
-write-host "app id/name: $appId/$displayName"
-write-host "app uri: $appUri"
+appId=$(az ad app list --query [].appId -o tsv)
+displayName=$(az ad app list --query [].displayName -o tsv)
+appUri=$(az ad app list --query [].identifierUris[0] -o tsv)
+echo "app id/name: $appId/$displayName"
+echo "app uri: $appUri"
 
 # insert variables into template, prepared as terraform values
-$template = Get-Content "terraform.tfvars.template" -Raw
-$expanded = Invoke-Expression "@`"`r`n$template`r`n`"@"
-write-host $expanded | Set-Content -Path terraform.tfvars -Encoding UTF8
-write-host "`nWRITTEN: terraform.tfvars"
+sedcmd=''
+for var in subscriptionId appId thepassword tenantId ;do
+  printf -v sc 's/$%s/%s/;' $var "${!var//\//\\/}"
+  sedcmd+="$sc"
+done
+sed -e "$sedcmd" <terraform.tfvars.template > terraform.tfvars
+echo -e "\nWRITTEN: terraform.tfvars"
 
